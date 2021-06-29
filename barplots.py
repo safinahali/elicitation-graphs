b84c464e541b228b8ba524d91b2aa6908190d33b
Safinah Ali <safinahaali@gmail.com>
2021-06-29 11:31:51 -0400

    web view

--- barplots.py
+++ barplots.py
@@ -4,7 +4,7 @@ import pandas as pd
 df = pd.read_csv('cf_norm_sim.csv')
 
 a = 0.2
-b = 0.8
+b = 0.4
 
 threshold_val = a
 threshold_val_second = (3*a+b)/4
@@ -18,7 +18,7 @@ fp_confusion_second = df.loc[df["threshold"] == threshold_val_second, "fp"]
 tp_confusion_second = df.loc[df["threshold"] == threshold_val_second, "tp"]
 fn_confusion_second = df.loc[df["threshold"] == threshold_val_second, "fn"]
 
-x = ["a", "(3a+b)/4"]
+x = ["Threshold = a", "Threshold = (3a+b)/4"]
 
 fig = go.Figure()
 
@@ -27,4 +27,18 @@ fig.add_trace(go.Bar(x=x, y=[fp_confusion.iat[0], fp_confusion_second.iat[0]], m
 fig.add_trace(go.Bar(x=x, y=[(fn_confusion.iat[0]*-1), (fn_confusion_second.iat[0]*-1)], marker_color='crimson', name='False Negative'))
 
 fig.update_layout(barmode='relative', title_text='Confusion Matrix Graph')
-fig.show()
+# fig.show()
+fig.write_html("stackedbars.html")
+
+
+barclass = go.Figure(data=[
+	go.Bar(x=x, y=[fp_confusion.iat[0], fp_confusion_second[0]], marker_color='crimson', name='False Positive'),
+	go.Bar(x=x, y=[tp_confusion.iat[0], tp_confusion_second[0]], marker_color='deepskyblue', name='True Positive'),
+	go.Bar(x=x, y=[(fn_confusion.iat[0]*-1), (fn_confusion_second[0]*-1)], marker_color='crimson', name='False Negative')
+	])
+
+
+barclass.show()
+barclass.update_layout(barmode='group')
+barclass.write_html("barclass.html")
+
